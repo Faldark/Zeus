e50e2d307e0965ed465d5f2c3c03ed6f9192d1fc
Bogdan Chukhno <bogdan.chukhno@quantumit.com.au>
2020-01-31 16:29:28 +0200

    minor fix.

--- Zeux.Test.Server/ClientApp/src/app/my-assets/RouteResolver.ts
+++ Zeux.Test.Server/ClientApp/src/app/my-assets/RouteResolver.ts
@@ -20,7 +20,8 @@ export class RouteResolver implements Resolve<any> {
   resolve(route: ActivatedRouteSnapshot) {
     var type = route.params.type;
     if (!this.assets) {
-      return this.http.get<Array<Asset>>('/api/asset/Get/All', this.httpOptions).pipe(map(data => this.assets = data));
+      return this.http.get<Array<Asset>>('/api/asset/Get/All', this.httpOptions)
+      .pipe(map(data => this.assets = data.filter(x => type === 'all' ? x : x.assetTypeName === type)));
     }
     else {
       return of(this.assets.filter(x => type === 'all' ? x : x.assetTypeName === type));
